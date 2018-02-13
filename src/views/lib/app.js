9bdc13c978ec6c305aa5d1f4d34d08491da28145
inunotaisho26 <ebfairweather@gmail.com>
2018-02-13 13:48:57 -0500

    pushing core changes to

--- src/views/lib/app.js
+++ src/views/lib/app.js
@@ -4,7 +4,7 @@ import angular from 'angular';
 import ngResource from 'angular-resource';
 import ngRoute from 'angular-route';
 import navbar from '../navbar/navbar.dir.js';
-import footer from '..'
+import foot from '../footer/footer.dir.js';
 import home from '../home/home.js';
 import portfolio from '../portfolio/portfolio.js';
 import about from '../about/about.js';
@@ -14,7 +14,7 @@ import portfolioTemplate from '../portfolio/portfolio.html';
 import aboutTemplate from '../about/about.html';
 import contactTemplate from '../contact/contact';
 
-var myApp = angular.module('myApp', ['ngRoute', 'ngResource', 'home', 'portfolio', 'about', 'contact']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngResource', 'navbar', 'home', 'portfolio', 'about', 'contact', 'foot']);
     myApp.config(function($routeProvider, $locationProvider){
         $routeProvider
         .when('/home', {
